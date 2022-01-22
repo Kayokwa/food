621004c82682fdfe1f51622f50eddbd005f1f7ff
Kayokwa Chibuye <kayokwa.chibuye@gmail.com>
2022-01-22 00:35:30 -0800

    updated searchApi and calling it using useEffect

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { SearchBar } from "../components/SearchBar";
 import yelp from "../api/yelp";
@@ -7,12 +7,12 @@ function SearchScreen() {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
   const [erroMessage, setErrorMessage] = useState("");
-  async function searchApi() {
+  async function searchApi(searchTerm) {
     try {
       const response = await yelp.get("/search", {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: "san jose",
         },
       });
@@ -22,9 +22,17 @@ function SearchScreen() {
       setErrorMessage(err);
     }
   }
+
+  useEffect(() => {
+    searchApi("pasta");
+  });
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
       {erroMessage ? <Text>{erroMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
