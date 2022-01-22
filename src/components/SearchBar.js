a7ed484b14b2bc63948f7364baa27f6dd2f7df4c
Kayokwa Chibuye <kayokwa.chibuye@gmail.com>
2022-01-21 22:53:56 -0800

    Added more styling to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,7 @@ import { Feather } from "@expo/vector-icons";
 export function SearchBar() {
   return (
     <View style={styles.backgroundStyle}>
-      <Feather name="search" size={30} />
+      <Feather name="search" style={styles.iconStyle} />
       <TextInput placeholder="Search" style={styles.inputStyle} />
     </View>
   );
@@ -13,6 +13,7 @@ export function SearchBar() {
 
 const styles = StyleSheet.create({
   backgroundStyle: {
+    marginTop: 15,
     backgroundColor: "#F0EEEE",
     height: 50,
     borderRadius: 5,
@@ -21,8 +22,13 @@ const styles = StyleSheet.create({
   },
 
   inputStyle: {
-    borderColor: "black",
-    borderWidth: 1,
     flex: 1,
+    fontSize: 18,
+  },
+
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
   },
 });
