ef49236d2942c4a89afa2b4577f22ecca026262a
Kayokwa Chibuye <kayokwa.chibuye@gmail.com>
2022-01-22 01:55:49 -0800

    showed images with basic styling

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 export function ResultsDetail({ result }) {
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Image style={styles.imageStyle} source={{ uri: result.image_url }} />
+      <Text style={styles.name}>{result.name}</Text>
+      <Text>
+        {result.rating} Stars, {result.review_count} Reviews
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageStyle: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+  },
+  name: {
+    fontWeight: "bold",
+  },
+});
