b4479f2a17ebff242c8d1c63abe8c707b9950fa1
Kayokwa Chibuye <kayokwa.chibuye@gmail.com>
2022-01-22 01:19:36 -0800

    basic list display of results

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
 export function ResultList({ title, results }) {
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
-      <Text>Results: {results.length}</Text>
+      <FlatList
+        horizontal
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 }
