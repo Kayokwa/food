e344ddd2d7ef50586d05f2fc2729ebfe5cedd3af
Kayokwa Chibuye <kayokwa.chibuye@gmail.com>
2022-01-23 19:15:24 -0800

    removed API Key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
-    Authorization:
-      "Bearer swBaOCOQsfOrJ3V6HOWPh9C2YciVmk4uhpz4AMvfHCApE0vBzzHX_oSUJHCgKsemg8QBh5JXwNhvbAO32NvWUtrlyjkbSM1THeSQNppa-Zf_wVXkBXVAx-mzLIrrYXYx",
+    Authorization: "Bearer <Add Yelp API Key Here>",
   },
 });
